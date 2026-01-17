7bfb44eb5e45b713e168251d759710750748114a
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-17 13:31:14 +0530

    add new feature card section

--- main.js
+++ main.js
@@ -153,3 +153,10 @@ window.sendToWhatsApp = function() {
     // Open WhatsApp
     window.open(whatsappURL, '_blank');
 };
+
+
+function changePrice(el,newP,oldP){
+  const card = el.closest(".card");
+  card.querySelector(".new-price").innerText = "₹" + newP + ".00";
+  card.querySelector(".old-price").innerText = "₹" + oldP + ".00";
+}
